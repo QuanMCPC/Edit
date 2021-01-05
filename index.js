dd2fe85dad437164d4ae38037f2f3860696cd09e
QuanMCPC <60545346+QuanMCPC@users.noreply.github.com>
2021-01-05 22:18:40 +0700

    .

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
 const { menu } = require("./menu")
-const { autoUpdater } = require("electron-updater")
 const isWindows = process.platform === "win32";
 app.whenReady().then(() => {
     const myWindow = new BrowserWindow({
@@ -16,7 +15,7 @@ app.whenReady().then(() => {
         frame: isWindows ? false : true,
         minWidth: 320,
         minHeight: 240,
-        icon: "./icon.ico"
+        icon: "./icon.png"
     });
     myWindow.webContents.executeJavaScript(`var cachedText = ""`)
     myWindow.loadFile("index.html")
@@ -29,19 +28,4 @@ app.whenReady().then(() => {
             });
         }
     });
-    myWindow.once("ready-to-show", () => {
-        autoUpdater.checkForUpdatesAndNotify();
-    })
 })
-ipcMain.on('app_version', (event) => {
-    event.sender.send('app_version', { version: app.getVersion() });
-});
-autoUpdater.on('update-available', () => {
-    mainWindow.webContents.send('update_available');
-});
-autoUpdater.on('update-downloaded', () => {
-    mainWindow.webContents.send('update_downloaded');
-});
-ipcMain.on('restart_app', () => {
-    autoUpdater.quitAndInstall();
-  });
